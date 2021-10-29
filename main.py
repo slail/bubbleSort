b8f3b25dda393fd89413b68fe96ff48a7fb72c52
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-29 16:27:56 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def bubbleSort(array):
+    """
+    A function that takes in a array of intergers and returns a sorted version that array. Making use of the Bubble
+    Sort algorithm to sort that array.
+    """
+    pass

854e5f5f5e43161f0555756eb44ccda1301edc07
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-29 16:33:14 -0400

    added bubbleSort algo

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ def bubbleSort(array):
     A function that takes in a array of intergers and returns a sorted version that array. Making use of the Bubble
     Sort algorithm to sort that array.
     """
-    pass
+    isSorted = False
+    while not isSorted:
+        isSorted = True
+        for idx in range(len(array) - 1):
+            if array[idx + 1] < array[idx]:
+                swap(idx, idx + 1, array)
+                isSorted = False
+    return array
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
